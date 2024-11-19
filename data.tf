8a8fa5c8fc2b8404ba9716af947772659c33ffaf
Daniil Vasylenko <daniel.vsln@gmail.com>
2024-11-19 15:05:13 +0200

    feat: change aws ami type (#3)

--- data.tf
+++ data.tf
@@ -47,15 +47,17 @@ data "aws_ami" "this" {
   owners      = ["591542846629"]
 
   filter {
-    name   = "name"
-    values = ["amzn2-ami-ecs-hvm-*"]
+    name   = "architecture"
+    values = ["arm64"]
   }
-
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
   filter {
     name   = "root-device-type"
     values = ["ebs"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
